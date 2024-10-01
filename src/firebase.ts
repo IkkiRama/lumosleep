76c49bc9ff746263eb9b9e250df4276f88ad432d
IkkiRama <rifkivamaus@gmail.com>
2024-10-01 20:14:31 +0700

    Update firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -6,12 +6,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDZ3kXVbxPeYsABsynZJxKLnKzfdK-nGxI",
-  authDomain: "nocturn-f10ff.firebaseapp.com",
-  projectId: "nocturn-f10ff",
-  storageBucket: "nocturn-f10ff.appspot.com",
-  messagingSenderId: "96246454913",
-  appId: "1:96246454913:web:7f08d9da21ff7977670b80"
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MSGID,
+  appId: import.meta.env.VITE_FIREBASE_APPID
+
 };
 
 // Initialize Firebase
